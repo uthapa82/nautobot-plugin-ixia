a147ba8b71bdcd935fa6b1bf9f4891f4f888d2d0
uthapa82 <tupendra022@gmail.com>
2023-02-21 23:15:30 -0500

    ixia24 complete

--- nautobot_plugin_ixia/tables.py
+++ nautobot_plugin_ixia/tables.py
@@ -6,14 +6,42 @@
 # __status__ = "development"
 #------------------------------
 
-from nautobot.utilities.tables import BaseTable
+import django_tables2 as tables 
+from nautobot.utilities.tables import BaseTable, ToggleColumn
+
 from .models import IxiaRow24, IxiaRow14, IxiaAppServer
 
 #table definition using Nautobot base template
 class IxiaRow24Table(BaseTable):
-
+    
+    #creates alias name 
+    pk = ToggleColumn()
+    module = tables.LinkColumn(verbose_name="Module Number")
+    description = tables.Column(verbose_name="Description/Project")
+    
     class Meta(BaseTable.Meta):
         model = IxiaRow24
+        
+        #determines which field to display
+        fields = (
+            "pk",
+            "module",
+            "speed",
+            "port",
+            "status",
+            "tenant",
+            "description",
+        )
+        #determines default columns 
+        default_columns = (
+            "pk",
+            "module",
+            "speed",
+            "port",
+            "status",
+            "tenant",
+            "description"
+        )
 
 class IxiaRow14Table(BaseTable):
     
